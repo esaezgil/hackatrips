6306abf87c9ebc5d70062c388e91318602b970bf
Enrique Saez <enrique.saez@skyscanner.net>
2018-01-21 10:33:31 +0100

    asda

--- backend/backend.py
+++ backend/backend.py
@@ -11,12 +11,21 @@ app = Flask(__name__)
 
 HOTELS_COMBINED_AUTOSUGGEST = "http://sandbox.hotelscombined.com/api/2.0/search/full?query={city}&limit=10&languageCode=EN&countryCode=ES&apiKey={apikey}"
 
-HOTELS_COMBINED_SEARCH = "http://sandbox.hotelscombined.com/api/2.0/hotels?destination={user_destination}&checkin={ci}&checkout={co}&rooms={rooms}&apiKey={api_key}&sessionID={session_id}"
+HOTELS_COMBINED_SEARCH = "http://sandbox.hotelscombined.com/api/2.0/hotels?destination={user_destination}&checkin={ci}&checkout={co}&rooms={rooms}&apiKey={api_key}&sessionID={session_id}&starRating={starRating}"
 
 HOTELS_COMBINED_BOOKING = "https://www.hotelscombined.com/{redirect_path}"
 
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko)"
 HC_HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko)'}
+
+PRICE_SENSITIVITY_MAPPING = {
+    0: 5,
+    1: 4,
+    2: 3,
+    3: 2,
+    2: 1,
+    1: 1
+}
 
 
 @app.route('/')
@@ -30,13 +39,14 @@ def get_user_info(user_id):
 @app.route('/search/<city>/<sensitivity>')
 def get_custom_hotels(city, sensitivity):
     place_id = get_hotels_combined_place_id(city)
-    hotel_results = get_hotels_combined_results(place_id)
+    recommended_hotel_cat = PRICE_SENSITIVITY_MAPPING[sensitivity]
+    hotel_results = get_hotels_combined_results(place_id, starRating=recommended_hotel_cat)
 
-    pois = 
+    # pois =
 
     final_response = {}
     final_response['hotels'] = hotel_results
-    final_response['pois'] =
+    # final_response['pois'] =
     return json.dumps(hotel_results)
 
 
@@ -58,16 +68,12 @@ def request_til_complete(url, headers=None):
         hotels_results_json = json.loads(hotels_results._content)
     return hotels_results_json
 
-def get_hotels_combined_results(place_id, guests=2, rooms=1, checkin='2018-04-03', checkout='2018-04-06'):
+def get_hotels_combined_results(place_id, guests=2, rooms=1, checkin='2018-04-03', checkout='2018-04-06', starRating=4):
     session_id = uuid.uuid4()
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko)'}
-    hotels_combined_search_url = HOTELS_COMBINED_SEARCH.format(user_destination=place_id, ci=checkin, co=checkout, rooms=rooms, api_key=HC_KEY, session_id=session_id)
-    # hotels_results = requests.get(hotels_combined_search_url, headers=headers)
-    # hotels_results_json = json.loads(hotels_results._content)
-    # import ipdb; ipdb.set_trace()
-    # while not hotels_results_json['isComplete']:
-    #     hotels_results = requests.get(hotels_combined_search_url, headers=headers)
-    #     hotels_results_json = json.loads(hotels_results._content)
+    hotels_combined_search_url = HOTELS_COMBINED_SEARCH.format(user_destination=place_id, ci=checkin, co=checkout,
+                                                               rooms=rooms, api_key=HC_KEY, session_id=session_id,
+                                                               starRating=starRating)
     hotels_results_json = request_til_complete(hotels_combined_search_url, HC_HEADERS)
     hotels_list = hotels_results_json['results']
 
@@ -97,4 +103,4 @@ def get_hotel_details(href):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=3000)
--- backend/templates/user_infos.py
+++ backend/templates/user_infos.py
@@ -6,15 +6,64 @@ user_infos["marcos"] = \
         "user_type": "Single",
         "cities": [
             {
-                "name": "roma",
-                "img": "http://www.telegraph.co.uk/content/dam/video_previews/l/n/lnmmzznze6qxo0ddc4ajydmzur-2xcfh-xlarge.jpg",
-                "lat": 40.4200522,
-                "long": -3.7068298
+                "name": "Roma",
+                "img": "http://www.rometransfer.eu/wp-content/uploads/2016/07/promo-01-boscolo-roma.jpg",
+                "lat": 41.902783,
+                "long": 12.496366
             },
             {"name": "Paris",
              "img": "https://cache-graphicslib.viator.com/graphicslib/thumbs360x240/2050/SITours/eiffel-tower-dinner-and-seine-river-cruise-in-paris-459739.jpg",
              "lat": 40.4200525,
              "long": -3.7068292
+             },
+            {"name": "Praga",
+             "img": "http://www.rumbo.es/hoteles/img/republica-checa/praga.jpg",
+             "lat": 50.075538,
+             "long": 14.4378
+             },
+            {"name": "Berlin",
+             "img": "http://sumfinity.com/wp-content/uploads/2013/12/Brandenburg-Gate-West-Berlin.jpg",
+             "lat": 52.520007,
+             "long": 13.404954
+             },
+            {"name": "London",
+             "img": "https://static.pexels.com/photos/50632/pexels-photo-50632.jpeg",
+             "lat": 51.507351,
+             "long": -0.127758
+             }
+        ],
+        "sensitivity": 0
+    }
+user_infos = {}
+user_infos["pablo"] = \
+    {
+        "user_type": "Family",
+        "cities": [
+            {
+                "name": "Budapest",
+                "img": "https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5515999951001_5214933106001-vs.jpg?pubId=5104226627001&videoId=5214933106001",
+                "lat": 47.497912,
+                "long": 3.7068298
+            },
+            {"name": "Milan",
+             "img": "https://www.100resilientcities.org/wp-content/uploads/2017/06/Milan-hero-crop_2.jpg",
+             "lat": 45.464204,
+             "long":  9.189982
+             },
+            {"name": "Lyon",
+             "img": "http://ca.france.fr/sites/default/files/imagecache/ATF_Image_bandeau_v2/quartier_saint-georgesvieux_lyonlyon-tourisme-tristan_deschamps_16.jpg",
+             "lat": 45.764043,
+             "long": 4.835659
+             },
+            {"name": "Amsterdam",
+             "img": "https://www.iamexpat.nl/sites/default/files/styles/article--full/public/amsterdam-netherlands.jpg?itok=UNsvHedg",
+             "lat": 52.370216,
+             "long": 4.895168
+             },
+            {"name": "Copenhague",
+             "img": "https://ep01.epimg.net/elviajero/imagenes/2017/01/16/actualidad/1484583700_664198_1484652533_sumario_normal.jpg",
+             "lat": 55.676097,
+             "long": 12.568337
              }
         ],
         "sensitivity": 0
